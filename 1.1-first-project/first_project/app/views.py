d655aaaba99b3bc3fa4044c7aa4a55be1ed95b74
Anna Galochkina <galochkina_anya@mail.ru>
2025-08-09 14:21:44 +0300

    Delete

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,35 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import datetime
+import os
 
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
+    
     pages = {
-        'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Домашняя страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
     return render(request, template_name, context)
 
 
-def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+def current_time_view(request):
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    result = f'Текущее время: {current_time}'
+    return HttpResponse(result)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+    files = os.listdir()
+    # Создаем HTML-список файлов
+    file_list = "<ul>" + "".join([f"<li>{file}</li>" for file in files]) + "</ul>"
+    return HttpResponse(f"Содержимое рабочей директории:{file_list}")
+
